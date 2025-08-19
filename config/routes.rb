84942f77c329f97f9cfa824207b4537cd869fe4c
ricaellenascimento <ricaellenascimento@gmail.com>
2025-08-19 20:34:25 -0300

    Implementação da rota de cadastro de usuários

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,23 @@
 Rails.application.routes.draw do
-  # Define rotas padrão para o controller Atividades
+  # Rota para verificar a saúde da aplicação
+  get "up" => "rails/health#show", as: :rails_health_check
+
+  get 'signup', to: 'users#new', as: 'signup'
+  post 'signup', to: 'users#create'
+
+  get 'get_all_users', to: 'users#get_all_users'
+
+
+  # Definições das rotas para os recursos
+  resources :tarefas
+  resources :categorias
+  resources :lembretes
+  resources :metas
   resources :atividades
   resources :users
   resources :planner_views
   resources :statuses
 
+  # Defina a rota raiz ("/") se desejar
+  # root "tarefas#index"  # ou outro controlador e ação que você preferir
 end
