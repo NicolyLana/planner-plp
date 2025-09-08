d6417d7ce46b538dc6f39e59f9edfed0831e0d02
NicolyLana <nicolylana.dev@gmail.com>
2025-09-08 20:50:06 -0300

    Alteração no layout de esqueceu senha

--- app/javascript/login.js
+++ app/javascript/login.js
@@ -1,16 +1,11 @@
-// login.js
 $(document).ready(function() {
     "use strict";
 
-    /*==================================================================
-    [ Focus Input ]*/
     $('.input-field, .inputnew').each(function(){
-        // Se já tem valor ao carregar a página
         if($(this).val().trim().length > 0) {
             $(this).addClass('has-val');
         }
 
-        // Ao sair do campo
         $(this).on('blur', function(){
             if($(this).val().trim().length > 0) {
                 $(this).addClass('has-val');
@@ -20,8 +15,6 @@ $(document).ready(function() {
         });
     });
 
-    /*==================================================================
-    [ Validate Form ]*/
     var input = $('.validate-input .input-field, .validate-input .inputnew');
 
     $('.validate-form').on('submit', function(){
@@ -42,8 +35,6 @@ $(document).ready(function() {
         });
     });
 
-    /*==================================================================
-    [ Validate Input Function ]*/
     function validate(input) {
         if($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
             if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
@@ -57,8 +48,6 @@ $(document).ready(function() {
         return true;
     }
 
-    /*==================================================================
-    [ Show / Hide Validate ]*/
     function showValidate(input) {
         var thisAlert = $(input).parent();
         $(thisAlert).addClass('alert-validate');
